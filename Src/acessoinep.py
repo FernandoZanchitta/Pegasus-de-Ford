5cf3a480bca502c556dadcec9ee60c97b66acffd
FernandoZanchitta <50930741+FernandoZanchitta@users.noreply.github.com>
2021-04-07 15:43:57 -0300

    Validando cidade no site qedu

--- Src/acessoinep.py
+++ Src/acessoinep.py
@@ -1,22 +1,18 @@
 from selenium import webdriver
 import time
 
-# search_string = input("Input da pesquisa: ")
-# search_string = search_string.replace(' ', '+')
+
 PATH = "/Users/FernandoZanchitta/Documents/chromedriver"
 driver = webdriver.Chrome(PATH)
-# for i in range(1):
-#     matched_elements = browser.get("https://www.google.com/search?q=" +
-#                                      search_string + "&start=" + str(i))
+qedu_url = "https://www.qedu.org.br/escola/36163-upaon-educacional-ltda/censo-escolar"
+driver.get(url=qedu_url)
 
-driver.get(url="https://www.qedu.org.br/escola/279164-morumbi-escola-unidade-moema/sobre")
-
-incategory = driver.find_elements_by_xpath('//*[@class="table table-striped"]/tbody/tr/td')[0].text
-
+inep = driver.find_elements_by_xpath('//*[@class="table table-striped"]/tbody/tr/td')[0].text
+city = driver.find_elements_by_xpath('//*[@class="subnav-title"]/ul/li [3]')[0].text
 #CodigoInep = incategory.find_element_by_tag_name("tbody").find_element_by_tag_name("tr").find_element_by_tag_name("td")
 
-print(incategory)
-time.sleep(5)
+print(inep + ' , ' + city)
+time.sleep(2)
 driver.close()
 # driver.exit(driver)
 # TODO:Comparar o nome da escola procurada
