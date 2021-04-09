a26b7741d18df6be9104f79a2c69241525c1e55b
FernandoZanchitta <50930741+FernandoZanchitta@users.noreply.github.com>
2021-04-09 14:45:03 -0300

    colocando acessoinep.py e pesquisa_google.py no mesmo arquivo para facilitar

--- src/acessoinep.py
+++ src/acessoinep.py
@@ -1,21 +1,37 @@
 from selenium import webdriver
 import time
 
-
 # search_string = search_string
+
+
+def acessoinep(escola,cidade):
+    qedu_url = pesquisa_google(escola,cidade)
+    driver.get(url=qedu_url)
+
+    inep = driver.find_elements_by_xpath('//*[@class="table table-striped"]/tbody/tr/td')[0].text
+    city = driver.find_elements_by_xpath('//*[@class="subnav-title"]/ul/li [3]')[0].text
+    #CodigoInep = incategory.find_element_by_tag_name("tbody").find_element_by_tag_name("tr").find_element_by_tag_name("td")
+
+    print(inep + ' , ' + city)
+    time.sleep(2)
+    driver.close()
+def pesquisa_google(escola,cidade):
+    escola = escola.replace(' ', '+')
+    cidade = cidade.replace(' ', '+')
+    query = escola + "+" + cidade
+    search_url = "https://www.google.com/search?q=" + query + "+qedu"
+    driver.get(url=search_url)
+    elems = driver.find_elements_by_css_selector(".yuRUbf [href]")
+    links = [elem.get_attribute('href') for elem in elems]
+    aux = 0
+    while links[aux].find('http://www.qedu.org.br/escola/') == -1:
+        aux += 1
+    time.sleep(2)
+    return(links[aux])
+# driver.exit(driver)
+
+escola = input("Nome da Escola:\n")
+cidade = input("Cidade da escola\n")
 PATH = "/Users/FernandoZanchitta/Documents/chromedriver"
 driver = webdriver.Chrome(PATH)
-# for i in range(1):
-#     matched_elements = browser.get("https://www.google.com/search?q=" +
-#                                      search_string + "&start=" + str(i))
-qedu_url = "https://www.qedu.org.br/escola/36163-upaon-educacional-ltda/censo-escolar"
-driver.get(url=qedu_url)
-
-inep = driver.find_elements_by_xpath('//*[@class="table table-striped"]/tbody/tr/td')[0].text
-city = driver.find_elements_by_xpath('//*[@class="subnav-title"]/ul/li [3]')[0].text
-#CodigoInep = incategory.find_element_by_tag_name("tbody").find_element_by_tag_name("tr").find_element_by_tag_name("td")
-
-print(inep + ' , ' + city)
-time.sleep(2)
-driver.close()
-# driver.exit(driver)
+acessoinep(escola,cidade)
